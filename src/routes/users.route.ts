dd5341415d01142552a1b9073db763f077dc1a61
RafaelDanelluci <rafaeldanelluci@yahoo.com.br>
2022-02-07 15:39:19 -0300

    Add PUT and Delete method

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -18,4 +18,13 @@ usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
   res.status(StatusCodes.CREATED).send(newUser);
 });
 
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  res.status(StatusCodes.OK).send({ uuid });
+});
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.OK);
+});
+
 export default usersRoute;
