fad47c6dbe0d47734cb5f8dfd38e46fee97cacae
RafaelDanelluci <rafaeldanelluci@yahoo.com.br>
2022-02-11 10:06:00 -0300

    Create a method for input new data to db

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -22,10 +22,27 @@ class UserRepository {
 
     const values = [uuid];
 
-    const { rows } = await db.query<User>(query);
+    const { rows } = await db.query<User>(query, values);
     const [ user ] = rows;
     
     return user;
+  }
+
+  async create(user: User): Promise<string> {
+    const script = `
+      INSERT INTO application_user (
+        username,
+        password
+      )
+      VALUES($1, crypt($2, 'my_salt'))
+      RETURNING uuid
+    `;
+
+
+    const values = [user.username, user.password];
+    const { rows } = await db.query<{ uuid: string }>(script, values);
+    const [ newUser ] = rows;
+    return newUser.uuid;
   }
 
 }
