e2cd5929d559257476f9bb0ab5e3f778cb799ce9
RafaelDanelluci <rafaeldanelluci@yahoo.com.br>
2022-02-11 12:01:06 -0300

    Create error handler and mofied what was needed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Express from 'express';
+import errorHandler from './middlewares/error-handler.middlewares';
 import statusRoute from './routes/status.router';
 import usersRoute from './routes/users.route';
 
@@ -11,6 +12,9 @@ app.use(Express.urlencoded({ extended: true }));
 //config de rotas
 app.use(statusRoute);
 app.use(usersRoute);
+
+//config dos Handlers de erro
+app.use(errorHandler);
 
 //Inicializaçao do servidor
 app.listen(3000, () => {
