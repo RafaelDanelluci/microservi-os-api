77e8aaa66e8f9103676bfdb99873bf387704285d
RafaelDanelluci <rafaeldanelluci@yahoo.com.br>
2022-02-15 10:54:42 -0300

    remove the DBLink

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgres://cskvfjkn:MVX3kIOsPc5EoI1Ggpip8cABPuXko16O@kesavan.db.elephantsql.com/cskvfjkn';
+const connectionString = '';
 const db = new Pool({ connectionString });
 
 export default db;
